84e52667f633d108c5b8d3ee38654df50a768bf1
Borislav Stoyanov <bostoycontact@gmail.com>
2021-03-07 14:32:42 +0200

    refactor board component

--- src/containers/Board.jsx
+++ src/containers/Board.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import Lane from '../components/Lane/Lane'
-
+import withDataFetching from '../withDataFetching'
 
 const BoardWrapper = styled.div`
   display: flex;
@@ -13,53 +13,23 @@ const BoardWrapper = styled.div`
     flex-direction: column;
   }
 `;
-
-export default function Board() {
-    const [data, setData] = useState([])
-    const [isLoading, setLoading] = useState(true)
-    const [error, setError] = useState('')
-
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const tickets = await fetch(`../../assets/data.json`)
-                const ticketsJSON = await tickets.json()
-                if (ticketsJSON) {
-                    setData(ticketsJSON)
-                    setLoading(false)
-                }
-            } catch (error) {
-                setLoading(false)
-                setError(error.message)
-            }
-            return
-        }
-
-        fetchData()
-
-    }, [])
-
-    const lanes = [
-        { id: 1, title: 'To-Do' },
-        { id: 2, title: 'In Progress' },
-        { id: 3, title: 'Review' },
-        { id: 4, title: 'Done' }
-    ]
-    console.log('Data: ', data)
-    console.log('isLoading: ', isLoading)
-    console.log('error: ', error)
-
-
+const Board = ({ lanes, isLoading, error, data }) => {
     return (
         <BoardWrapper>
-            {lanes.map(lane =>
-                <Lane
-                    key={lane.id}
-                    title={lane.title}
-                    loading={isLoading}
-                    error={error}
-                    tickets={data.filter(ticket => ticket.lane === lane.id)}
-                />)}
+            {lanes.map(lane => {
+                return (
+                    <Lane
+                        key={lane.id}
+                        title={lane.title}
+                        isLoading={isLoading}
+                        error={error}
+                        tickets={data.filter(ticket => ticket.lane === lane.id)}
+                    />)
+
+            })}
         </BoardWrapper>
     )
 }
+
+export default withDataFetching(Board)
+
