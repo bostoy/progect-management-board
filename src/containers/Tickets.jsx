db51fc1dbd45e53266c312887ea6cfd5a7caf68f
Borislav Stoyanov <bostoycontact@gmail.com>
2021-03-07 15:03:36 +0200

    use hoc to display tickets

---
+++ src/containers/Tickets.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+import withDataFetching from '../withDataFetching'
+import Ticket from '../components/Ticket/Ticket'
+
+const TicketsWrapper = styled.div`
+display:flex;
+justify-content: space-betweem;
+flex-direction:row;
+margin: 5%;
+
+@media(max-width: 768px){
+    flex-direction:column;
+}
+`
+const Alert = styled.div`
+text-align: center;
+`
+
+const Tickets = ({ isLoading, data, error }) => {
+    return (
+        <TicketsWrapper>
+            {(isLoading || error) && <Alert>{isLoading ? 'Loading...' : error}</Alert>}
+            {data.map(ticket => <Ticket key={ticket.id} ticket={ticket} />)}
+        </TicketsWrapper>)
+}
+
+export default withDataFetching(Tickets)
