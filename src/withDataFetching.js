fb740b92f73ade755ad7da3d9cb300a1c265a640
Borislav Stoyanov <bostoycontact@gmail.com>
2021-03-07 14:21:38 +0200

    create hoc

---
+++ src/withDataFetching.js
@@ -0,0 +1,47 @@
+import { Component } from 'react'
+
+export default function withDataFetching(WrappedComponent) {
+
+    return class extends Component {
+        constructor(props) {
+            super(props)
+            this.state = {
+                data: [],
+                isLoading: true,
+                error: ''
+            }
+        }
+
+        async componentDidMount() {
+            try {
+                const tickets = await fetch(`../../assets/data.json`)
+                const ticketsJSON = await tickets.json()
+
+                if (ticketsJSON) {
+                    this.setState({
+                        data: ticketsJSON,
+                        isLoading: false,
+                    })
+                }
+            } catch (error) {
+                this.setState({
+                    isLoading: false,
+                    error: error.message
+                })
+            }
+
+        }
+        render() {
+
+            const { data, isLoading, error } = this.state
+
+            return (<WrappedComponent
+                data={data}
+                isLoading={isLoading}
+                error={error}
+                {...this.props}
+            />)
+        }
+
+    }
+}
